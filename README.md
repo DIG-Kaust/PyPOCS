90d4cf74b13eba45dd426a72ab62c85828425b98
mrava87 <matteoravasi@gmail.com>
2023-07-12 13:48:23 +0300

    Finalized script and added test data

--- README.md
+++ README.md
@@ -7,18 +7,35 @@ Reproducible material for **Why POCS works, and how to make it better - Ravasi M
 This repository is organized as follows:
 
 * :open_file_folder: **pypocs**: python library containing routines for POCS interpolation with various solvers;
-* :open_file_folder: **data**: folder containing data;
+* :open_file_folder: **data**: folder containing sample data;
 * :open_file_folder: **notebooks**: set of jupyter notebooks reproducing the experiments in the paper (see below for more details);
 * :open_file_folder: **scripts**: set of python scripts used to run multiple experiments ...
 
 ## Notebooks
 The following notebooks are provided:
 
+- :orange_book: ``POCS_Overthrust-DataCreation.ipynb``: notebook creating the input .npz dataset to be used as input for the scripts (see below);
 - :orange_book: ``POCS_Overthrust-Irregular3D.ipynb``: notebook performing interpolation of a shot gather of the Overthrust dataset from irregularly sampled inlines;
 - :orange_book: ``POCS_Overthrust-FullIrregular3D.ipynb``: notebook performing interpolation of a shot gather of the Overthrust dataset from an irregularly sampled 2d-grid;
 - :orange_book: ``POCS_Overthrust-Summary.ipynb``: notebook visualizing the results of the various algorithms used in the two previous notebooks.
 - :orange_book: ``POCS_Overthrust-Offthegrid3D_windows.ipynb``: notebook performing interpolation of a shot gather of the Overthrust dataset from irregularly sampled inlines with dithering (using disjoint windowed approach);
 - :orange_book: ``POCS_Overthrust-Offthegrid3D_sliding.ipynb``: notebook performing interpolation of a shot gather of the Overthrust dataset from irregularly sampled inlines with dithering (using global, sliding-window approach).
+
+## Scripts
+The following script is provided:
+
+- :orange_book: ``InterpOffthegrid.py``: script performing interpolation with primal-dual on off-the-grid receivers;
+
+The script requires an input ``.npz`` file containing the following fields. 
+
+- :card_index: ``data``: 3-dimensional pressure data to interpolate of size ``nrx x nry x nt``
+- :card_index: ``t``: time axis of size ``nt``.
+- :card_index: ``RECX``: x-locations of all receivers in the input regular grid.
+- :card_index: ``RECY``: y-locations of all receivers in the input regular grid.
+- :card_index: ``recz``: z-location of receivers (assumed to be here the same for all receivers - use average if they differ).
+- :card_index: ``SRCX``: x-location of source.
+- :card_index: ``SRCY``: y-location of source.
+- :card_index: ``srcz``: z-location of source.
 
 ## Getting started :space_invader: :robot:
 To ensure reproducibility of the results, we suggest using the `environment.yml` file when creating an environment.
--- scripts/InterpOffthegrid.py
+++ scripts/InterpOffthegrid.py
@@ -32,7 +32,6 @@ from pypocs.POCS import POCS
 from pypocs.metric import *
 from pypocs.interpolation import Callback
 
-np.random.seed(5)
 
 def main(parser):
 
@@ -43,21 +42,26 @@ def main(parser):
         setup = yaml.load(stream, Loader=yaml.FullLoader)
 
     filename = setup['exp']['inputfile']
+    outfilename = setup['exp']['outputfile']
     plotflag = setup['exp']['plotflag']
 
-    nry = setup['data']['nry']
-    nrx = setup['data']['nrx']
-    nt = setup['data']['nt']
     dry = setup['data']['dry']
     drx = setup['data']['drx']
     dt = setup['data']['dt']
 
     perc_subsampling = setup['subsampling']['perc_subsampling']
+    seed = setup['subsampling']['seed']
 
+    threshmask = setup['preprocessing']['threshmask']
+    itoff = setup['preprocessing']['itoff']
     vshift = setup['preprocessing']['vshift']
     nwin = setup['preprocessing']['nwin']
     nover = setup['preprocessing']['nover']
     nop = setup['preprocessing']['nop']
+    epsweighting = setup['preprocessing']['epsweighting']
+    sigmaweighting = setup['preprocessing']['sigmaweighting']
+    threshweighting = setup['preprocessing']['threshweighting']
+    scweighting = setup['preprocessing']['scweighting']
     nfft = setup['preprocessing']['nfft']
 
     niter = setup['interpolation']['niter']
@@ -78,51 +82,45 @@ def main(parser):
         for key, value in setup[section].items():
             print(f'{key} = {value}')
         print('\n-------------------------------\n')
-    print('-------------------------------\n')
+
+    np.random.seed(seed)
 
     ######### Data loading and preprocessing #########
 
+    fdata = np.load(filename)
+
     # Load data
-    nrxorig, nryorig = 177, 90
-    data = np.fromfile(filename, dtype='float32')
-    data = data.reshape(nt + 75, nrxorig * nryorig)[75:]
-    data = data.reshape(nt, nryorig, nrxorig).transpose(2, 1, 0)  # y, x, t
-    data = data[:nrx, :nry, :500] / np.max(np.abs(data[:nrx, :nry, :500]))
-    ns, nr, nt = data.shape
-    t = np.arange(0, nt) * dt
+    data = fdata['data']
+    nrx, nry, nt = data.shape
+    t = fdata['t']
+
+    # Normalize data
+    data = data / np.max(np.abs(data))
 
     # Acquisition
-    isrc = 3338 * 4  # selected source
-    ny, nx, nz = 200, 330, 155
-    y, x, z = np.arange(ny) * 15., np.arange(nx) * 15., np.arange(nz) * 15.
-    srcx = np.arange(300, x[-1] - 300, 20)
-    srcy = np.arange(300, y[-1] - 300, 20)
-
-    SRCY, SRCX = np.meshgrid(srcy, srcx, indexing='ij')
-    SRCX, SRCY = SRCX.ravel(), SRCY.ravel()
-
-    recx = np.arange(700, x[-1] - 700, 20)[:nrx]
-    recy = np.arange(600, y[-1] - 600, 20)[:nry]
-
-    RECY, RECX = np.meshgrid(recy, recx, indexing='ij')
-    RECX, RECY = RECX.ravel(), RECY.ravel()
+    RECX = fdata['RECX']
+    RECY = fdata['RECY']
+    recz = fdata['recz']
+    SRCX = fdata['SRCX']
+    SRCY = fdata['SRCY']
+    srcz = fdata['srcz']
 
     # Subsampling locations
-    Nsub = int(np.round(ns * perc_subsampling))
-    iy = np.arange(ns)
-    ix = np.arange(nr)
+    Nsub = int(np.round(nrx * perc_subsampling))
+    iy = np.arange(nrx)
+    ix = np.arange(nry)
 
     iygrid, ixgrid = np.meshgrid(iy, ix, indexing='ij')
 
-    iyava_y = np.random.randint(2, ns - 2, Nsub)
-    iyava = np.tile(iyava_y, (nr, 1)).ravel()
-    ixava = np.tile(np.arange(nr), (Nsub, 1)).T.ravel()
+    iyava_y = np.random.randint(2, nrx - 2, Nsub)
+    iyava = np.tile(iyava_y, (nry, 1)).ravel()
+    ixava = np.tile(np.arange(nry), (Nsub, 1)).T.ravel()
 
-    mask2d = np.zeros((ns, nr))
+    mask2d = np.zeros((nrx, nry))
     mask2d[iyava, ixava] = 1
     iava2d = np.where(mask2d.ravel() == 1)[0]
 
-    mask3d = np.zeros((ns, nr, nt))
+    mask3d = np.zeros((nrx, nry, nt))
     mask3d[iyava, ixava, :] = 1
     iava3d = np.where(mask3d.ravel() == 1)[0]
 
@@ -131,39 +129,21 @@ def main(parser):
     Nsub = int(len(iava3d) / nt)
 
     # Create restriction operator
-    Rop = Restriction(ns * nr * nt, iava=iava3d.ravel(), dtype='float64')
-    dottest(Rop)
-    mask = Rop.H * Rop * np.ones_like(data).ravel()
-    mask = mask.reshape(ns, nr, nt)
+    Rop = Restriction(nrx * nry * nt, iava=iava3d.ravel(), dtype='float64')
 
     # Create bilinear operator
     iyava_pert = iyava + np.random.uniform(-0.5, 0.5, Nsub)
     ixava_pert = ixava + np.random.uniform(-0.5, 0.5, Nsub)
     ixava_pert[ixava_pert < 0] = 0.
-    ixava_pert[ixava_pert > nr - 2] = nr - 2
+    ixava_pert[ixava_pert > nry - 2] = nry - 2
     iava3d1 = np.vstack((iyava_pert, ixava_pert))
 
-    Bop = Bilinear(cp_asarray(iava3d1), (ns, nr, nt), dtype='float64')
-    dottest(Bop, backend='cupy')
-
-    Bop1 = Bilinear(iava3d1, (ns, nr), dtype='float64')
-    BopBopH = (Bop1 @ Bop1.H).todense()
-
-    print(Rop, Bop)
-
-    # Geometry
-    fig, ax = plt.subplots(1, 1, figsize=(16, 12))
-    ax.scatter(iygrid, ixgrid, c='k')
-    ax.scatter(iyava, ixava, c='r')
-    ax.scatter(iava3d1[0], iava3d1[1], c='b')
-    ax.set_title('Geometry')
-    plt.show()
+    Bop = Bilinear(cp_asarray(iava3d1), (nrx, nry, nt), dtype='float64')
+    print(f'Restriction operator {Rop}, Bilinear Operator: {Bop}')
 
     # Off-grid data
-    datasub = Bop * cp_asarray(data)
-    datamasked = Bop.H * datasub
-
-    datasub = cp_asnumpy(datasub)
+    datasub = Bop @ cp_asarray(data)
+    datamasked = Bop.H @ datasub
     datamasked = cp_asnumpy(datamasked)
 
     # FK spectra
@@ -174,10 +154,12 @@ def main(parser):
 
     # Create direct arrival mask
     direct_mask = np.ones_like(data)
-    direct_thresh = 0.02 * np.max(np.abs(data))
+    direct_thresh = threshmask * np.max(np.abs(data))
     for iy in range(nrx):
         for ix in range(nry):
-            direct_mask[iy, ix, :max(0, np.where(np.abs(data[iy, ix]) > direct_thresh)[0][0] - 10)] = 0.
+            it = np.where(np.abs(data[iy, ix]) > direct_thresh)[0]
+            if len(it) > 0:
+                direct_mask[iy, ix, :max(0, it[0] - itoff)] = 0.
 
     # Patched fk sparsyfing transform
     dimsd = data.shape
@@ -189,30 +171,27 @@ def main(parser):
     Srecop = Patch3D(F1op.H, dims, dimsd, nwin, nover, nop1, tapertype='cosine')
 
     # Shift operator
-    shift = np.sqrt((300 - 10) ** 2 +
-                    (RECX.reshape(nry, nrx) - SRCX[isrc]) ** 2 +
-                    (RECY.reshape(nry, nrx) - SRCY[isrc]) ** 2) / vshift
+    shift = np.sqrt((recz - srcz) ** 2 +
+                    (RECX.reshape(nry, nrx) - SRCX) ** 2 +
+                    (RECY.reshape(nry, nrx) - SRCY) ** 2) / vshift
 
     Shiftop = Shift((nrx, nry, nt), shift=-shift.T, sampling=dt, axis=2)
 
     # Shift data
     datashifted = np.real(Shiftop * data)
-    datareshifted = np.real(Shiftop.H * datashifted)
 
     # Shift subsampled data
-    datashiftedsub = Bop * cp_asarray(datashifted)
-    datashiftedmasked = Bop.H * datashiftedsub
-
-    datashiftedsub = cp_asnumpy(datashiftedsub)
+    datashiftedsub = Bop @ cp_asarray(datashifted)
+    datashiftedmasked = Bop.H @ datashiftedsub
     datashiftedmasked = cp_asnumpy(datashiftedmasked)
 
     datashiftedfk = FFTop @ datashifted
     datashiftedmaskedfk = FFTop @ datashiftedmasked
 
     # Find normalization factors
-    weightshifted = 1. / (sp.ndimage.gaussian_filter(np.abs(sp.signal.hilbert(datashifted)), sigma=4) + 1e-3)
-    weightshifted[:, :200][weightshifted[:, :200] > 300] = 300  # remove infinite values when there is no signal
-    weightshifted /= (weightshifted.max() / 20)
+    weightshifted = 1. / (sp.ndimage.gaussian_filter(np.abs(sp.signal.hilbert(datashifted)), sigma=sigmaweighting) + epsweighting)
+    weightshifted[weightshifted > threshweighting] = threshweighting  # remove infinite values when there is no signal
+    weightshifted /= (weightshifted.max() / scweighting)
 
     # Apply weighting with subsampled data
     datashiftedsub = Bop * cp_asarray(datashifted * weightshifted)
@@ -231,9 +210,6 @@ def main(parser):
     xpd = \
         PrimalDual(laff, lort, Srecop.H, x0=Bop.H * cp_asarray(datashiftedsub.ravel()),
                    tau=tau, mu=mu, niter=niter, gfirst=True, show=False, callback=cb)
-
-    ppd = FFTop * xpd.ravel()
-    ppd = ppd.reshape(nfft, nfft, nfft // 2 + 1)
     errpd = [np.linalg.norm(datashifted - datashiftedmasked / weightshifted) / np.linalg.norm(
         datashifted)] + callback.err
     snr_hist_pd = 20 * np.log10(1 / np.array(errpd))
@@ -241,10 +217,16 @@ def main(parser):
     # Renormalize and shift back reconstructed data
     datarec_pdshiftedback = cp_asnumpy(np.real(Shiftop.H * (xpd.reshape(nrx, nry, nt) / cp_asarray(weightshifted))))
 
-    print(metrics(data, direct_mask*datarec_pdshiftedback, itmin=0, verb=True))
+    # Final SNR
+    _ = metrics(data, direct_mask*datarec_pdshiftedback, verb=True)
+
+    ######### Saving results #########
+    if outfilename is not None:
+        np.savez(outfilename, data=direct_mask * datarec_pdshiftedback, t=t,
+                 RECX=RECX, RECY=RECY, SRCX=SRCX, SRCY=SRCY,
+                 snr_hist=snr_hist_pd[::jsrnsave])
 
     ######### Plotting #########
-
     if plotflag:
         # Geometry
         fig, ax = plt.subplots(1, 1, figsize=(16, 12))
@@ -253,56 +235,65 @@ def main(parser):
         ax.scatter(iava3d1[0], iava3d1[1], c='b')
         ax.set_title('Geometry')
 
-        explode_volume(data.transpose(2, 0, 1), x=96, clipval=(-0.2, 0.2), figsize=(8, 8), title='Full data')
-        explode_volume(datamasked.transpose(2, 0, 1), x=96, clipval=(-0.2, 0.2), figsize=(8, 8),
+        # Data (full and subsampled)
+        explode_volume(data.transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2), figsize=(8, 8), title='Full data')
+        explode_volume(datamasked.transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2), figsize=(8, 8),
                        title='Subsampled data')
 
-        explode_volume(np.fft.fftshift(np.abs(datafk[..., :55]), axes=(0, 1)).transpose(2, 0, 1), t=25,
-                       tlim=[0, FFTop.fs[-1][55]], tlabel='f(Hz)',
+        # FK spectra
+        explode_volume(np.fft.fftshift(np.abs(datafk[..., :100]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+                       tlim=[0, FFTop.fs[-1][100]], tlabel='f(Hz)',
                        cmap='jet', figsize=(8, 8), clipval=(0, 0.2))
 
-        explode_volume(np.fft.fftshift(np.abs(datamaskedfk[..., :55]), axes=(0, 1)).transpose(2, 0, 1), t=30,
-                       tlim=[0, FFTop.fs[-1][55]], tlabel='f(Hz)',
+        explode_volume(np.fft.fftshift(np.abs(datamaskedfk[..., :100]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+                       tlim=[0, FFTop.fs[-1][100]], tlabel='f(Hz)',
                        cmap='jet', figsize=(8, 8), clipval=(0, 0.2))
 
+        # Direct wave mask
         explode_volume(direct_mask.transpose(2, 0, 1), x=30, cmap='gray_r', clipval=(0, 1), figsize=(8, 8),
                        title='Direct Mask')
         explode_volume((direct_mask * data).transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2), figsize=(8, 8),
                        title='Masked data')
 
+        # Data Shift
         plt.figure(figsize=(8, 3))
         plt.imshow(shift)
         plt.axis('tight')
         plt.colorbar()
         plt.title('Shift')
 
-        explode_volume(data.transpose(2, 0, 1), t=40, x=95, y=45, clipval=(-0.2, 0.2), figsize=(8, 8), title='Data')
-        explode_volume(datashifted.transpose(2, 0, 1), t=40, x=95, y=45, clipval=(-0.2, 0.2), figsize=(8, 8),
+        explode_volume(datashifted.transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2), figsize=(8, 8),
                        title='Shifted data')
 
-        explode_volume(np.fft.fftshift(np.abs(datashiftedfk[..., :70]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+        # FK spectra of shifted data
+        explode_volume(np.fft.fftshift(np.abs(datashiftedfk[..., :100]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+                       tlim=[0, FFTop.fs[-1][100]], tlabel='f(Hz)',
                        cmap='jet', figsize=(8, 8), clipval=(0, 0.2))
 
-        explode_volume(np.fft.fftshift(np.abs(datashiftedmaskedfk[..., :70]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+        explode_volume(np.fft.fftshift(np.abs(datashiftedmaskedfk[..., :100]), axes=(0, 1)).transpose(2, 0, 1), t=30,
+                       tlim=[0, FFTop.fs[-1][100]], tlabel='f(Hz)',
                        cmap='jet', figsize=(8, 8), clipval=(0, 0.2))
 
-        explode_volume(weightshifted.transpose(2, 0, 1), x=96, figsize=(8, 8), clipval=(0, 10),
+        # Data weighting
+        explode_volume(weightshifted.transpose(2, 0, 1), x=30, figsize=(8, 8), clipval=(0, 10),
                        cmap='jet', title='Data weight')
 
-        explode_volume((datashifted * weightshifted).transpose(2, 0, 1), x=96, figsize=(8, 8), clipval=(-0.2, 0.2),
+        explode_volume((datashifted * weightshifted).transpose(2, 0, 1), x=30, figsize=(8, 8), clipval=(-0.2, 0.2),
                        title='Weighted data')
 
-        explode_volume(data.transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2), figsize=(8, 8), title='Data')
+        # Inversion
         explode_volume((direct_mask * datarec_pdshiftedback).transpose(2, 0, 1), x=30, clipval=(-0.2, 0.2),
-                       figsize=(8, 8), title='PD')
+                       figsize=(8, 8), title='PD Reconstruction')
         explode_volume((direct_mask * datarec_pdshiftedback).transpose(2, 0, 1) - data.transpose(2, 0, 1), x=30,
                        clipval=(-0.2, 0.2), figsize=(8, 8), title='Error')
 
+        # SNR evolution
         fig, ax = plt.subplots(1, 1, figsize=(6, 5))
         ax.plot(np.arange(0, niter + 1, jsrnsave), snr_hist_pd[::jsrnsave], 'k', lw=3)
         ax.set_title('SNR')
 
         plt.show()
+
 
 if __name__ == "__main__":
     description = '3D POCS Interpolation with off-the-grid receivers'
